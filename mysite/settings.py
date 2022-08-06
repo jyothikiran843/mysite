fce8559b74dd3d6d04e83d0a5deb97cdf4be722c
jyothikiran843 <jyothikiranthammana843@gmail.com>
2022-08-06 11:49:51 +0530

    commit#45

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,10 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS=[
-    BASE_DIR/'base/static'
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
